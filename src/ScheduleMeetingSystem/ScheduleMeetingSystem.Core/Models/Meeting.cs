d064df8bb03df4e2c29dd3f90828cef28fedf5f0
Anatolii-Borshch <anatoliiborshch@gmail.com>
2025-08-07 16:57:11 +0300

    Created initial project structure, and base entities.

---
+++ src/ScheduleMeetingSystem/ScheduleMeetingSystem.Core/Models/Meeting.cs
@@ -0,0 +1,12 @@
+﻿namespace ScheduleMeetingSystem.Core.Models
+{
+    public class Meeting
+    {
+        public long Id { get; set; }
+        public string Title { get; set; } = String.Empty;
+        public DateTime StartTime { get; set; }
+        public DateTime EndTime { get; set; }
+        
+        public ICollection<User> Users { get; set; }
+    }
+}
---
+++ src/ScheduleMeetingSystem/ScheduleMeetingSystem.Core/Models/User.cs
@@ -0,0 +1,11 @@
+﻿namespace ScheduleMeetingSystem.Core.Models
+{
+    public class User
+    {
+        public long Id { get; set; }
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        
+        public ICollection<Meeting> Meetings { get; set; }
+    }
+}
